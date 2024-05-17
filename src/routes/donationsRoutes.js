32884e72211d82e08f718d18dddffdbc0e09ee7c
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-05-17 18:54:41 -0300

    feat: Add getDonationById, updateDonation, and deleteDonation routes
    This commit adds the `getDonationById`, `updateDonation`, and `deleteDonation` routes to the `donationsRoutes.js` file. These routes enable retrieving a donation by its ID, updating a donation's status, and deleting a donation from the database, respectively. This update enhances the functionality of the donation management system.

--- src/routes/donationsRoutes.js
+++ src/routes/donationsRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const donationsController = require('../controllers/donationsController');
 
 router.get('/donations', donationsController.getAllDonations);
-// router.get('/donations/:id', donationsController.getDonationById);
+router.get('/donations/:id', donationsController.getDonationById);
 router.post('/donations', donationsController.createDonation);
+router.put('/donations/:id', donationsController.updateDonation);
+router.delete('/donations/:id', donationsController.deleteDonation);
 
 module.exports = router;
