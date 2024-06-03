74b541eac08d0d62cc069048a00bcda3d9e2b663
Matheszambondacosta <matheus-costa@aluno.senai.br>
2024-06-03 14:38:47 -0300

    feat: Atualizar controlador e rotas para presentes

--- src/routes/giftRoutes.js
+++ src/routes/giftRoutes.js
@@ -1,12 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const giftController = require('../controllers/giftController');
 
-router.get('/gift', giftController.getAllGift);
-router.get('/gift/id/:id', giftController.getGiftById);
-router.post('/gift', giftController.createGift);
-router.put('/gift/:id', giftController.updateGift);
-router.delete('/gift/:id', giftController.deleteGift);
-
-
-module.exports = router;
