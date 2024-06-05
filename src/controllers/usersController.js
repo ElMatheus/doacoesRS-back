95aa54e950d036781665d6584130ec2f6a37c9a4
Matheus Gomes <matheus.goncalves5@aluno.senai.br>
2024-06-05 10:46:24 -0300

    feat: verification for name user

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -67,8 +67,12 @@ async function createUser(req, res) {
   try {
     const { name, email, password } = req.body;
     const userAlreadyExists = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    const userAlreadyExistsName = await pool.query('SELECT * FROM users WHERE name = $1', [name])
     if (userAlreadyExists.rowCount !== 0) {
       return res.status(400).json({ message: 'Email já cadastrado' });
+    }
+    if (userAlreadyExistsName.rowCount !== 0) {
+      return res.status(400).json({ message: 'Nome de usuário já existe' });
     }
     const passwordHash = await hash(password, 8);
     const result = await pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', [name, email, passwordHash]);
