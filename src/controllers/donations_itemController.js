dedca3ceb0101957c80e93f545c22a038083a55f
ElMatheus <matheus.goncalves5@aluno.senai.br>
2024-06-09 21:10:30 -0300

    chore: Add routes and controllers for retrieving donations items by donation ID and calculating full price

--- src/controllers/donations_itemController.js
+++ src/controllers/donations_itemController.js
@@ -16,6 +16,44 @@ async function getAllDonationsItems(req, res) {
     }
 }
 
+async function getAllDonationsItemsByDonationId(req, res) {
+    try {
+        const { id } = req.params;
+        const result = await pool.query('SELECT donations_items.id as donation_item_id, donations_items.donation_id, donations_items.product_id, donations_items.quantity, donations.donation_date, products.name as product_name, products.image AS product_image, products.value AS product_value, users.name as user_name FROM donations_items INNER JOIN donations ON donations_items.donation_id = donations.id INNER JOIN products ON donations_items.product_id = products.id INNER JOIN users ON donations.user_id = users.id WHERE donations_items.donation_id = $1', [id]);
+        if (result.rowCount === 0) {
+            return res.status(404).send('Itens de doação não encontrados');
+        }
+        res.json({
+            status: 'success',
+            message: 'Itens de doação encontrados',
+            total: result.rowCount,
+            data: result.rows
+        });
+    }
+    catch (error) {
+        return res.status(500).send('Erro ao buscar itens de doação');
+    }
+}
+
+async function getFullPriceByDonationId(req, res) {
+    try {
+        const { id } = req.params;
+        const result = await pool.query('SELECT SUM(products.value * donations_items.quantity) as full_price FROM donations_items INNER JOIN products ON donations_items.product_id = products.id WHERE donations_items.donation_id = $1', [id]);
+        if (result.rowCount === 0) {
+            return res.status(404).send('Itens de doação não encontrados');
+        }
+        res.json({
+            status: 'success',
+            message: 'Preço total dos itens de doação encontrado',
+            total: result.rowCount,
+            data: result.rows
+        });
+    }
+    catch (error) {
+        return res.status(500).send('Erro ao buscar itens de doação');
+    }
+}
+
 async function createDonationItem(req, res) {
     try {
         const { donation_id, product_id, quantity } = req.body;
@@ -41,5 +79,7 @@ async function createDonationItem(req, res) {
 
 module.exports = {
     getAllDonationsItems,
-    createDonationItem
+    createDonationItem,
+    getAllDonationsItemsByDonationId,
+    getFullPriceByDonationId
 };
